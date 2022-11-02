f321e176212c73b913a41ac282e46a6b4cbe1482
URK21CS2037 <109648094+URK21CS2037@users.noreply.github.com>
2022-11-02 14:05:00 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,57 +11,27 @@ class DoublyCircularLinkedList:
         self.count = 0
 
     def add_at_tail(self, data) -> bool:
-        # Write code here
+        new_node = Node(data)
+        if self.count > 0:
+            new_node.previous = self.end
+            self.end.next = new_node
+            self.head.previous = new_node
+            new_node.next = self.head
+        else:
+            self.head = new_node
+        self.end = new_node
+        self.count += 1
+        return True
 
     def add_at_head(self, data) -> bool:
-        # Write code here
-
-    def add_at_index(self, index, data) -> bool:
-        # Write code here
-
-    def get(self, index) -> int:
-        # Write code here
-
-    def delete_at_index(self, index) -> bool:
-        # Write code here
-
-    def get_previous_next(self, index) -> list:
-        # Write code here
-
-
-# Do not change the following code
-operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
-input_data = input()
-data = []
-iteration_count = 0
-
-for item in input_data.split(', '):
-    inner_list = []
-    if item.isnumeric():
-        data.append(int(item))
-    elif item.startswith('['):
-        item = item[1:-1]
-        for letter in item.split(','):
-            if letter.isnumeric():
-                inner_list.append(int(letter))
-        data.append(inner_list)
-
-obj = DoublyCircularLinkedList()
-result = []
-for i in range(len(operations)):
-    if operations[i] == "add_at_head":
-        result.append(obj.add_at_head(data[i]))
-    elif operations[i] == "add_at_tail":
-        result.append(obj.add_at_tail(data[i]))
-    elif operations[i] == "add_at_index":
-        result.append(obj.add_at_index(int(data[i][0]), data[i][1]))
-    elif operations[i] == "get":
-        result.append(obj.get(data[i]))
-    elif operations[i] == "get_previous_next":
-        result.append(obj.get_previous_next(data[i]))
-    elif operations[i] == 'delete_at_index':
-        result.append(obj.delete_at_index(data[i]))
-
-print(result)
+        new_node = Node(data)
+        if self.count > 0:
+            new_node.next = self.head
+            new_node.previous = self.end
+            self.head.previous = new_node
+            self.end.next = new_node
+        else:
+            self.end = new_node
+        self.head = new_node
+        self.count += 1
+        return True
